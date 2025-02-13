f7d2cb027967c35b1d2622e03889ed203d3e400b
Kamil Rudny <k.rudny1@gmail.com>
2025-02-13 20:57:51 +0100

    Added buttons

--- campaign-front/app/page.tsx
+++ campaign-front/app/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
+import { Button } from '@mui/material';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+      <div className="container flex flex-col items-center justify-center mx-auto max-w-screen-lg">
+        <div className="flex flex-col items-center gap-y-6 my-10">
+          <h1 className="text-7xl font-semibold">Campaign Manager</h1>
+          <h2 className="text-3xl">Available balance: <span className="text-green-500">5999PLN</span></h2>
+        </div>
+
+        <div className="flex gap-x-4">
+          <Button variant="outlined" color="primary" size="large">Add product</Button>
+          <Button variant="outlined" color="primary" size="large">Add campaign</Button>
+        </div>
+
+      </div>
   );
 }
